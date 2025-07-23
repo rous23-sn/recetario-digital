ad40c0972655b75513b00ae6dae47727f602fa2f
rous23-sn <1008820212@unajma.edu.pe>
2025-07-23 13:40:35 -0500

    Agrego archivo environment.ts para Firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCm3bfia_kcQLXMsVy6Nq8hvJYharMoZUQ",
+    authDomain: "recetario-digital-c0d49.firebaseapp.com",
+    projectId: "recetario-digital-c0d49",
+    storageBucket: "recetario-digital-c0d49.firebasestorage.app",
+    messagingSenderId: "918575745040",
+    appId: "1:918575745040:web:6b5b797ad2bdb6776f2295"
+  }
+};
